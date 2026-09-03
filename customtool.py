d4fd29d0119933397337ec034ecead8461b23837
piyush00868 <pk2857636@gmail.com>
2026-09-04 00:14:45 +0530

    custom tool callimg

--- customtool.py
+++ customtool.py
@@ -6,5 +6,9 @@ def get_greeting(name: str) -> str:  # type hinting for the input and output
     Generate a greeting message for the given name."""  # docstring for the tool
     return f"Hello {name}, welcome to AI world"
 
-result = get_greeting.invoke({"name": "John"})  # invoking the tool with input parameters
+result = get_greeting.invoke({"name": "Piyush"})  # invoking the tool with input parameters
 print(result)
+
+print(get_greeting.name)  # prints the name of the tool
+print(get_greeting.description)  # prints the description of the tool
+print(get_greeting.args)  # prints the input arguments of the tool
